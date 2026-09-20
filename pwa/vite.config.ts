06e6eae999e423a35f9bdae47586c3fdf6181663
Robvg9 <roberticohhh@gmail.com>
2026-09-19 21:02:30 -0400

    fix: use root Vite base for static asset paths

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/assets/',
+  base: '/',
   build: {
     outDir: process.env.APPDEPLOY_VITE_OUT_DIR || 'dist',
     sourcemap:
