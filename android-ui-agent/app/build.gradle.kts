46527fe24ee9686893a7d96b8abf8f6a62531a90
Robvg9 <roberticohhh@gmail.com>
2026-09-20 10:42:21 -0400

    chore(rwht): bump Android UI Agent version

--- android-ui-agent/app/build.gradle.kts
+++ android-ui-agent/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.robvg9.ariauiagent"
         minSdk = 26
         targetSdk = runnerSdk
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     compileOptions {
