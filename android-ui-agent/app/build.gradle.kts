143abba2d975017f9a013b31c532d8942db8e2b8
Robvg9 <roberticohhh@gmail.com>
2026-09-21 09:52:03 -0400

    chore(android): bump UI agent to 1.1.14

--- android-ui-agent/app/build.gradle.kts
+++ android-ui-agent/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.robvg9.ariauiagent"
         minSdk = 26
         targetSdk = runnerSdk
-        versionCode = 19
-        versionName = "1.1.13"
+        versionCode = 20
+        versionName = "1.1.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
