8048dfc5bc24ce318af8406eba58cd845a3620b0
Robvg9 <roberticohhh@gmail.com>
2026-09-21 16:21:01 -0400

    fix(android): restore local IPC IpcAuth.kt

---
+++ android-ui-agent/app/src/main/java/com/robvg9/ariauiagent/IpcAuth.kt
@@ -0,0 +1,7 @@
+package com.robvg9.ariauiagent
+
+object IpcAuth {
+    const val TOKEN = "4fa3c34b8093d0ac3633fd58ade90bc224827f2a7b21cf29f26931c637e34d76"
+    const val PORT = 45874
+    const val PATH = "/execute"
+}
